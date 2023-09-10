f542e59784b4518c329242ed67ea9c8b6a2035c6
Елизавета <elizaveta@MacBook-Air-Elizaveta.local>
2023-09-10 12:24:17 +0300

    ADDED: 14,15,16,17,18 homeworks

--- hm18.js
+++ hm18.js
@@ -1,9 +1,7 @@
-// За допомогою fetch треба зробити запит на будь-який сервіс з https://jsonplaceholder.typicode.com/. Це може бути /posts, /users або інші.
-
-// Зробити запит, розпарсити данні з json, та вивести у консоль результат.
-
 fetch('https://jsonplaceholder.typicode.com/posts')
   .then((response) => {
+
+
     return response.json();
   })
   .then((data) => {
